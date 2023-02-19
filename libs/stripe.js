d100f6f022ea8594bb4b662bc47adacc062b23e2
Vittorio <tonibusatta@gmail.com>
2023-02-19 18:57:15 +0400

    update stripe

--- libs/stripe.js
+++ libs/stripe.js
@@ -4,20 +4,23 @@ const domain = process.env.SHOPIFY_STORE_DOMAIN;
 export async function createStripeSession(checkoutId, lineItems) {
   try {
     const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-                line_items: lineItems.map((item) => {
-                  return {
-                    price_data: {
-                      currency: "eur",
-                      product_data: {
-                        name: item.title,
-                      },
-                      unit_amount: item.variantPrice * 100,
-                    },
-                    quantity: item.variantQuantity,
-                  };
-                }),
-                mode: "payment",
+      payment_method_types: ["card"],
+      line_items: lineItems.map((item) => {
+        return {
+          price_data: {
+            currency: "eur",
+            product_data: {
+              name: item.title,
+            },
+            unit_amount: item.variantPrice * 100,
+          },
+          quantity: item.variantQuantity,
+        };
+      }),
+      shipping_address_collection: {
+        allowed_countries: ['RE'] // Pays autorisés pour la livraison
+      },
+      mode: "payment",
       success_url: `https://${domain}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `https://${domain}/cart`,
       client_reference_id: checkoutId,
