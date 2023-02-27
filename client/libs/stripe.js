f1cb82e402729a3a5cab94fcd9b9632bb1447d6a
Vittorio <tonibusatta@gmail.com>
2023-02-27 20:12:36 +0400

    /

--- client/libs/stripe.js
+++ client/libs/stripe.js
@@ -1,7 +1,7 @@
-import Stripe from "stripe";
+// import Stripe from "./stripe";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_TEST_KEY, {
-  apiVersion: "2022-11-15",
-});
+// const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_TEST_KEY, {
+//   apiVersion: "2022-11-15",
+// });
 
-export default stripe;
+// export default stripe;
